5e57a02bb71adf6047f6be480f5ef2753e97c663
hpinedoh <hpinedoh@gmail.com>
2024-06-09 13:03:24 -0600

    nuevos archivos: Java/Main.class 	nuevos archivos: Java/Main.java

---
+++ Java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.print("Estoy aprendiendo java. \n");
+    System.out.print("Es sorprendente. \n");
+    System.out.println(3);
+    System.out.println(5*3);
+    char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+System.out.println(myVar1);
+System.out.println(myVar2);
+System.out.println(myVar3);
+    }
+}
